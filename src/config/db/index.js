c2098089d76351df300adcc1ed8e60dca6fc2896
vuquangdai0309 <dinhdaizd211@gmail.com>
2024-06-22 16:07:28 +0700

    docker

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 // const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'ocop_score',
+  host: process.env.DB_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
